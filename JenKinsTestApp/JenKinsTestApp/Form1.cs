499afff8439065631e2f46556306bb1e4cfb1ed9
Jiwook Kang <jwkang@sensor.co.kr>
2017-01-16 16:53:36 +0900

    Jenkins Test for Upload

---
+++ JenKinsTestApp/JenKinsTestApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JenKinsTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Sample_Test()
+        {
+            int i;
+            int number = 0; 
+            for( i = 0; i < 10; i++)
+            {
+                number += i; 
+            }
+        }
+    }
+}
